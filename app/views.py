a0597d904fa869f659e75673de5587775e2a2c5a
Guilherme Scaranse <guiscaranse@gmail.com>
2017-10-11 23:15:06 -0300

    Programação manhã e noite completa

--- app/views.py
+++ app/views.py
@@ -14,4 +14,7 @@ def hello():
                            manha_ter = teste.atividadesPorDia("24", "manha"),
                            manha_quar = teste.atividadesPorDia("25", "manha"),
                            manha_qui = teste.atividadesPorDia("26", "manha"),
-                           manha_sex = teste.atividadesPorDia("27", "manha"))
+                           manha_sex = teste.atividadesPorDia("27", "manha"),
+                           noite_seg = teste.atividadesPorDia("23", "noite"),
+                           noite_ter = teste.atividadesPorDia("24", "noite"),
+                           noite_quar = teste.atividadesPorDia("25", "noite"))
